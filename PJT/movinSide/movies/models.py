598081db988268a9b9b1b7df51c4a1095b8b5ae0
Kang Yechan <ghn05188@naver.com>
2023-05-17 15:05:53 +0900

    build: Model 수정
    Genre Model 추가
    Movie Model 수정
    genres.json, movies.json 파일 추가

--- PJT/movinSide/movies/models.py
+++ PJT/movinSide/movies/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
 class Actor(models.Model):
     name = models.CharField(max_length=100)
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    
 class Movie(models.Model):
+    adult = models.BooleanField()
+    backdrop_path = models.TextField()
+    genre_ids = models.ManyToManyField(Genre, related_name='movies')
     title = models.CharField(max_length=10)
     overview = models.TextField()
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    actors = models.ManyToManyField(Actor, related_name='movies')
+    release_date = models.TextField()
+    original_language = models.CharField(max_length=10)
+    original_title = models.TextField()
+    popularity = models.FloatField()
+    poster_path = models.TextField()
+    video = models.BooleanField()
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    #actors = models.ManyToManyField(Actor, related_name='movies')
 
 class Review(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     movie = models.ForeignKey(
         Movie, related_name='reviews', on_delete=models.CASCADE)
-
 
